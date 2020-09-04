2417682e3724167f50c9188cda82e8532d79dc3b
luyi <luyi5@shanghai-electric.com>
2020-09-04 16:19:29 +0800

    去除axios的括号

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState, useCallback, useRef } from 'react'
-import { axios } from 'axios'
+import  axios  from 'axios'
 
 export function useTableHooks(localKey, defaultdata) {
   //loading 状态
