cfdc49be2ad8df396a49c6b5dc1499d06b6c0189
saltovo <saltovo@aliyun.com>
2020-12-29 22:04:09 +0800

    docs: 添加注释

--- src/hooks/useDeepCompareState.ts
+++ src/hooks/useDeepCompareState.ts
@@ -1,8 +1,15 @@
 import { useState, useEffect } from 'react';
 
+/**
+ * 只有当当前值和之前值不一样时才会跟新当前值
+ * @param value 
+ * @returns {Boolean}
+ */
+
 export function useDeepCompareState(value: any) {
   const [deepCompareState, setDeepCompareState] = useState<any>(value);
 
+  //判断两个值是否不同
   const isDifferent = (compareValue: any, other: any): boolean => {
     if (Array.isArray(compareValue) && Array.isArray(other)) {
       return JSON.stringify(compareValue) !== JSON.stringify(other);
@@ -19,6 +26,7 @@ export function useDeepCompareState(value: any) {
         return true;
       }
       const compare = objProps.every((ele) => {
+        //递归来解决object中有值为array和object的情况
         return !isDifferent(compareValue[ele], other[ele]);
         // return compareValue[ele] === other[ele]
       });
